e78f941b9e76dd22a27a35345f5fd8c66a7e3638
junwon <wjdwnsdnjs13@naver.com>
2025-05-22 23:05:53 +0900

    🎉 start project 🎉

---
+++ src/main/java/com/server/overtime/OverTimeApplication.java
@@ -0,0 +1,13 @@
+package com.server.overtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/overtime/OverTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.overtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

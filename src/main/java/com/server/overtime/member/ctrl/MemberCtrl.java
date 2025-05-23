1f1a6fc8e78ac9177d7e807af38ebf7e2db34bfd
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-05-24 01:27:08 +0900

    refactor : cors 와일드카드 추가

--- src/main/java/com/server/overtime/member/ctrl/MemberCtrl.java
+++ src/main/java/com/server/overtime/member/ctrl/MemberCtrl.java
@@ -9,16 +9,11 @@ import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController("/api/members")
+@CrossOrigin(origins = "*")
 public class MemberCtrl {
 
     private final MemberSv memberSv;

4f28c6c9d706ab0772d004d24e02c43ad9eda7a1
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-05-24 00:19:32 +0900

    feat : 멤버 회원가입, 로그인, 탈퇴 추가

---
+++ src/main/java/com/server/overtime/member/sv/MemberSv.java
@@ -0,0 +1,58 @@
+package com.server.overtime.member.sv;
+
+import com.server.overtime.member.ctrl.req.AdminKey;
+import com.server.overtime.member.dao.MemberRepository;
+import com.server.overtime.member.dao.entity.MemberEntity;
+import com.server.overtime.member.exception.AdminException;
+import com.server.overtime.member.exception.KakaoException;
+import com.server.overtime.member.kakao.KakaoUserInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberSv {
+    @Value("${admin.key}")
+    private String adminKey;
+
+    private final MemberRepository memberRepository;
+
+    private final KakaoSv kakaoSv;
+
+    public Long registerMember(String accessToken) {
+        KakaoUserInfo kakaoUserInfo = kakaoSv.getKakaoUserInfo(accessToken);
+        if(idVertifiedUser(kakaoUserInfo)) return memberRepository.findByIdToken(kakaoUserInfo.getIdToken()).get().getId();
+        if(memberRepository.findByNickname(kakaoUserInfo.kakaoAccount.profile.getNickname()).isPresent()) return 0L;
+
+        return memberRepository.save(kakaoUserInfo);
+    }
+
+    public Long login(String accessToken) {
+        KakaoUserInfo kakaoUserInfo = kakaoSv.getKakaoUserInfo(accessToken);
+        if(idVertifiedUser(kakaoUserInfo)) return 0L;
+        MemberEntity memberEntity = memberRepository.findByIdToken(kakaoUserInfo.getIdToken()).orElseThrow(IllegalAccessError::new);
+        return memberEntity.getId();
+    }
+
+    public String getAccessToken(String idCode) {
+        String accessToken = kakaoSv.getKakaoAccessToken(idCode);
+        log.warn("accessToken : {}", accessToken);
+        return accessToken;
+    }
+
+    public void deleteMemberByMemberId(Long memberRowId, AdminKey adminKey) {
+        if (this.adminKey.equals(adminKey.getAdminKey())) memberRepository.deleteMemberByMemberId(memberRowId);
+        else throw new AdminException.ADMINKEY_UNAUTHORIZED();
+    }
+
+    private Boolean idVertifiedUser(KakaoUserInfo kakaoUserInfo) {
+        if(kakaoUserInfo.getIdToken() == 0L) throw new KakaoException.KAKAO_MEMBER_NOT_FOUND();
+        if(memberRepository.findByIdToken(kakaoUserInfo.getIdToken()).isPresent()) return true;
+        return false;
+    }
+
+
+}
